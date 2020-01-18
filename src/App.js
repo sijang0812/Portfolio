02eca66da853c926bf5d3d03d8877b72ae9ee0f1
sijang0812 <sijang0812@gmail.com>
2020-01-18 23:06:11 +0900

    예

--- src/App.js
+++ src/App.js
@@ -5,23 +5,27 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-        	Edit 
-        </p>
-        <a
-          className="App-link"
-          href="https://www.naver.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React s
-        </a>
-      </header>
+    	<div>
+	      <header className="App-header">
+	        <img src={logo} className="App-logo" alt="logo" />
+	        <p>
+	          Edit <code>src/App.js</code> and save to reload.
+	        </p>
+	        <a
+	          className="App-link"
+	          href="https://www.naver.com"
+	          target="_blank"
+	          rel="noopener noreferrer"
+	        >
+	          Learn React s
+	        </a>
+	      </header>
+   		</div>
+   		<div className="App-body">
+   			<p>
+   				test
+   			</p>
+   		</div>
     </div>
   );
 }
