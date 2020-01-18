afbd32cafa0bb383390e8b64545fbac4c3a6681e
sijang0812 <sijang0812@gmail.com>
2020-01-18 19:57:49 +0900

    링크 수정

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.naver.com"
           target="_blank"
           rel="noopener noreferrer"
         >
