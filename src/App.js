0c7662338c6735de4352fd42637386b69f1ffb42
sijang0812 <sijang0812@gmail.com>
2020-01-18 23:44:35 +0900

    지금 수정

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
 	          target="_blank"
 	          rel="noopener noreferrer"
 	        >
-	          Learn React s
+	          Please~~~
 	        </a>
 	      </header>
    		</div>
