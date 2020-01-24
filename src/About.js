9621a192b3747616ed0bfa81f726489bb0b35755
sijang0812 <sijang0812@gmail.com>
2020-01-25 00:36:28 +0900

    day 25 commit

--- src/About.js
+++ src/About.js
@@ -8,7 +8,17 @@ export default () => (
 	<section className="App-section-others">
 		<div className="Section-main-div">
 			<h6 className="title-font-en">About Me</h6>
-			<h className="font-en">What contents??</h>
+			
+			<h4 className="title-font-ko">Name</h4>
+			<h className="font-ko">장수인</h>
+			
+			<br></br><br></br><br></br><br></br>
+			<h4 className="title-font-ko">Education</h4>
+			<h className="font-ko">서울여자대학교 정보보호학과 전공</h>
+			
+			<br></br><br></br><br></br><br></br>
+			<h4 className="title-font-ko">Skill</h4>
+			<h className="font-ko"></h>
 		</div>
 	</section>
 )
