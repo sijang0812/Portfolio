1a79ea2401deca83f331546811295e95df2f16c6
sijang0812 <sijang0812@gmail.com>
2020-01-26 04:01:54 +0900

    Updates

---
+++ precache-manifest.840286ccce1fbcb9756ea807618e3461.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7e1821bbc176b22294183354018d233",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "d9b7a8fd4748be25c851",
+    "url": "/Portfolio/static/css/main.46fe2817.chunk.css"
+  },
+  {
+    "revision": "62458ea72ae106c9299c",
+    "url": "/Portfolio/static/js/2.115de09c.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/Portfolio/static/js/2.115de09c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d9b7a8fd4748be25c851",
+    "url": "/Portfolio/static/js/main.c9430b7b.chunk.js"
+  },
+  {
+    "revision": "198a9ee31b8bd05dd181",
+    "url": "/Portfolio/static/js/runtime-main.0265fdca.js"
+  },
+  {
+    "revision": "a803e97a7dc10b7037ed2fff7253a9e1",
+    "url": "/Portfolio/static/media/android.a803e97a.png"
+  },
+  {
+    "revision": "df7e79f2cab9d0ccdb62b1b1e9247f61",
+    "url": "/Portfolio/static/media/c.df7e79f2.png"
+  },
+  {
+    "revision": "9a31653b39cc0ef403afa73a7b57156f",
+    "url": "/Portfolio/static/media/github.9a31653b.png"
+  },
+  {
+    "revision": "62be9fb6e1d7166e9cbeeed913096752",
+    "url": "/Portfolio/static/media/jjava.62be9fb6.png"
+  },
+  {
+    "revision": "6b5e6f8d025f78dbfa2952142c93e2c5",
+    "url": "/Portfolio/static/media/linux.6b5e6f8d.png"
+  },
+  {
+    "revision": "598d07e7761f5bca3a3f78e95b6db422",
+    "url": "/Portfolio/static/media/mail.598d07e7.png"
+  },
+  {
+    "revision": "3da7523f762a885274dd966df0a04416",
+    "url": "/Portfolio/static/media/main.3da7523f.jpg"
+  },
+  {
+    "revision": "8d859602e4edf37f8e6fbce188479840",
+    "url": "/Portfolio/static/media/react.8d859602.png"
+  }
+]);
