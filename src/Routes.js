fca435b7d1a5d18f31ef7fced4a3100ca8262f51
sijang0812 <sijang0812@gmail.com>
2020-01-28 21:24:11 +0900

    day28-1 commit1

--- src/Routes.js
+++ src/Routes.js
@@ -15,6 +15,7 @@ function Routes() {
 			<Route path='/about' component={About} />
 			<Route path='/projects' component={Projects} />
 			<Route path='/contact' component={Contact} />
+			<Route component={Home} />
 		</Switch>
 	</Router>
 	);
